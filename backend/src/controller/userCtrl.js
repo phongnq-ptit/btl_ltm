881fd1529f54d5f0f95f8f80deb5338d22fe7405
phongnq-ptit <ngphonga4@gmail.com>
2022-04-20 15:30:36 +0700

    update

---
+++ backend/src/controller/userCtrl.js
@@ -0,0 +1,11 @@
+
+const userCtrl = {
+    userLogin: async (req, res) => {
+
+    },
+    userRegister: async (req, res) => {
+
+    }
+}
+
+export default userCtrl;
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,8 +1,14 @@
 import experss from 'express';
+import userCtrl from '../controller/userCtrl';
 
 const router = experss.Router();
 
 const initUserRoute = (app) => {
+    // dang nhap
+    router.post('/login', userCtrl.userLogin);
+
+    // dang ky
+    router.post('/register', userCtrl.userRegister);
 
     return app.use('/api', router);
 }
